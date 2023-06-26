98a32161e790d7eb4920a3bab7dd7ccec1960808
David Kim <silqwer@gmail.com>
2023-06-27 07:40:34 +0900

    feat: layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="py-2 text-sm text-center text-white bg-slate-900">
+      <p>{"Dont't forget to CODE your DREAM | All Right Reserved."}</p>
+    </footer>
+  );
+}
