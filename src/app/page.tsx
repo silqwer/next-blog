509767eae92d65fa55caf76b75718218f208e9ff
David Kim <silqwer@gmail.com>
2023-06-28 08:02:04 +0900

    feat: get posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import FeaturedPosts from "@/components/posts/FeaturedPosts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <FeaturedPosts />
     </section>
   );
 }
