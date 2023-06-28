6c9d0751c9be5cb5577277aac2dc6f5c385d9e6a
David Kim <silqwer@gmail.com>
2023-06-29 07:57:51 +0900

    feat: carousel posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "@/components/Hero";
+import CarouselPosts from "@/components/posts/CarouselPosts";
 import FeaturedPosts from "@/components/posts/FeaturedPosts";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
       <Hero />
       <FeaturedPosts />
+      <CarouselPosts />
     </section>
   );
 }
