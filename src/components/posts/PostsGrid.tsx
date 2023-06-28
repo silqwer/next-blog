e0cf387de541989eedcde35533527b6c71d316c9
David Kim <silqwer@gmail.com>
2023-06-29 07:29:26 +0900

    feat: post card

--- src/components/posts/PostsGrid.tsx
+++ src/components/posts/PostsGrid.tsx
@@ -1,5 +1,6 @@
 import { Post } from "@/service/posts";
 import React from "react";
+import PostCard from "./PostCard";
 
 type Props = {
   posts: Post[];
@@ -9,7 +10,9 @@ export default function PostsGrid({ posts }: Props) {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.path}>{post.title}</li>
+        <li key={post.path}>
+          <PostCard post={post} />
+        </li>
       ))}
     </ul>
   );
